351007564575cc41df2d48d5f63a648c7445dc40
jshvaron <jshva@icloud.com>
2023-04-22 19:52:42 -0500

    assigns Variables fo search button, input (searcher), output to p element, set function to take input and display at p element under search bar, listens for button click and pulls user input

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,15 +1,29 @@
 // Sets up API key
 const APIKey = '5072822e38b72f252227ac7250f60b68';
-
 // let city; allows placeholder for variable to be reassigned
 var city;
 
+// assigns Variables fo search button, input ('searcher'), output to p element
+const button = document.getElementById('btn');
+const citySearch = document.getElementById('searcher');
+const cityLog = document.getElementById('city-name');
+
+// takes input and displays at p element under search bar
+function getCity(cityInput){
+    cityLog.innerHTML = cityInput;
+}
+//  listens for button click and pulls user input to display in function above
+button.addEventListener('click', () => {
+    getCity(searcher.value);
+    console.log(searcher.value)
+
+});
 
 
-
+// gets weather from API based off user input 
 function getWeather() {
-   var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=5072822e38b72f252227ac7250f60b68";
-    fetch(requestUrl)
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=dallas&appid=5072822e38b72f252227ac7250f60b68" 
+    fetch(queryURL)
         .then(res => res.json())
         .then(data => console.log(data))
 };
