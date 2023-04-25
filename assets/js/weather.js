f30dac3ce8ddbd33692870f39c07bd57b08d6a36
jshvaron <jshva@icloud.com>
2023-04-25 14:45:42 -0500

    recreated doc event listner to see if dynamically fendered buttons are logging. Button value is logging correctly

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,44 +1,68 @@
 // Sets up API key
-const APIKey = '5072822e38b72f252227ac7250f60b68';
-// let city; allows placeholder for variable to be reassigned
-var city;
+$(document).ready(function() {
 
-// assigns Variables fo search button, input ('searcher'), output to p element
-const button = document.getElementById('btn');
-const citySearch = document.getElementById('searcher');
-const cityList = document.getElementById('city-list');
+    const APIKey = '5072822e38b72f252227ac7250f60b68';
+    // let city; allows placeholder for variable to be reassigned
+    var city;
+    
+    // assigns Variables fo search button, input ('searcher'), output to p element
+    const searchbutton = document.getElementById('btn');
+    const citySearch = document.getElementById('searcher');
+    const cityList = document.getElementById('city-list');
+    // TODO: comment back in once function is ready
+    // const cityButtons = document.querySelectorAll('cityName');
+    const button = document.getElementsByTagName('button');
+    const MAX_CITY_SEARCH = 15; // Maximum number of searches to display
+    
+    console.log(button);
+        
+    
+    // waits for click, takes user input and renders a p tag that passes user input with a max of 15 searches. This max is set to fill the container appropriatly. Alert will populate a msg for premium content. That will be used for future implementation
+    searchbutton.addEventListener('click', () => {
+    
+      const pCount = cityList.querySelectorAll('button').length; // Get the number of button elements in the section
+      if (pCount < MAX_CITY_SEARCH) { // Check if the maximum limit is not reached
+        // if max is not reached, the following will create a new button for the user input 'city' to live in.
+        const input = citySearch.value;
+        const newCityButton = `<button class='cityName'>${input}</button`;
+        console.log(citySearch.value);
+        cityList.insertAdjacentHTML('beforeend', newCityButton);
+        citySearch.value = '';
+        
+
+      } else{
+        // lol
+        alert('To search the Weather in more cities, see our Premium Subsricption.')
+      };
+    
+      
+    });
+
+    // Queries clicks on the doc and logs the text inside the clicked el
+    document.addEventListener('click', function(event) {
+        if (event.target.classList.contains('cityName')) {
+          const cityName = event.target.textContent;
+          console.log(`Clicked on city: ${cityName}`);
+            // Make API request using cityName as input
+            // TODO:
+            // when button form cityList is clicked
+            // the value is = to lat and long
+            // if it = lat and long then it will pull the needed data
+        }
+    });
+
+    
+    
+    
+    // gets weather from API based off user input 
+    function getWeather() {
+        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=dallas&appid=5072822e38b72f252227ac7250f60b68" 
+        fetch(queryURL)
+            .then(res => res.json())
+            .then(data => console.log(data))
+    };
+    getWeather();
+    
 
 
-// waits for click, takes user input and renders a p tag that passes user input with a max of 9 searches. This max is set to fill the container appropriatly. Alert will populate a msg for premium content. That will be used for future implementation
-const MAX_CITY_SEARCH = 9; // Maximum number of searches to display
-button.addEventListener('click', () => {
-  const pCount = cityList.querySelectorAll('p').length; // Get the number of p elements in the section
-  if (pCount < MAX_CITY_SEARCH) { // Check if the maximum limit is not reached
-    const newcity = document.createElement('p'); // Create a new p element
-    newcity.textContent = citySearch.value; // Set the text content of the new city
-    cityList.appendChild(newcity); // Add the new city to the search area
-  } else{
-    // lol
-    alert('To search the Weather in more cities, see our Premium Subsricption.')
-  }
 });
-
-
-
-
-
-
-
-
-
-
-
-
-// gets weather from API based off user input 
-function getWeather() {
-    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=dallas&appid=5072822e38b72f252227ac7250f60b68" 
-    fetch(queryURL)
-        .then(res => res.json())
-        .then(data => console.log(data))
-};
-getWeather();
