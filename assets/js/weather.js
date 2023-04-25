57cb7ed5e2e87cc08e23923d3a66b1fe34903792
jshvaron <jshva@icloud.com>
2023-04-25 16:36:49 -0500

    fetches api city data from button.

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,18 +1,16 @@
 // Sets up API key
 $(document).ready(function() {
 
-    const APIKey = '5072822e38b72f252227ac7250f60b68';
-    // let city; allows placeholder for variable to be reassigned
-    var city;
-    
-    // assigns Variables fo search button, input ('searcher'), output to p element
+
+
     const searchbutton = document.getElementById('btn');
     const citySearch = document.getElementById('searcher');
     const cityList = document.getElementById('city-list');
-    // TODO: comment back in once function is ready
-    // const cityButtons = document.querySelectorAll('cityName');
     const button = document.getElementsByTagName('button');
-    const MAX_CITY_SEARCH = 15; // Maximum number of searches to display
+    const MAX_CITY_SEARCH = 10; // Maximum number of searches to display
+    // let city; allows placeholder for variable to be reassigned
+    var city;
+    city;
     
     console.log(button);
         
@@ -38,31 +36,35 @@ $(document).ready(function() {
       
     });
 
-    // Queries clicks on the doc and logs the text inside the clicked el
+    //  clicks on the doc log the text inside the clicked el(specifically buttons)
     document.addEventListener('click', function(event) {
         if (event.target.classList.contains('cityName')) {
           const cityName = event.target.textContent;
-          console.log(`Clicked on city: ${cityName}`);
+          const APIKey = '5072822e38b72f252227ac7250f60b68';
+          console.log(`Clicked on city: ${cityName} w/ ${APIKey}`);
             // Make API request using cityName as input
             // TODO:
             // when button form cityList is clicked
             // the value is = to lat and long
             // if it = lat and long then it will pull the needed data
-        }
+            // gets weather from API based off user input 
+            function getWeather() {
+                var queryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${APIKey}` 
+                fetch(queryURL)
+                    .then(res => res.json())
+                    .then(data => console.log(data))
+            };
+            getWeather();
+        
+        
+        };
     });
 
     
     
-    
-    // gets weather from API based off user input 
-    function getWeather() {
-        var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=dallas&appid=5072822e38b72f252227ac7250f60b68" 
-        fetch(queryURL)
-            .then(res => res.json())
-            .then(data => console.log(data))
-    };
-    getWeather();
-    
+    // `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${APIKey}` 
+
+    // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
 
 });
